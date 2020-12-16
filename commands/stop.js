4371db61fa39f0d3a4f3a18f306fab7702415783
SapphireVideos <mateo.hideg@hotmail.com>
2020-12-16 02:40:41 -0300

    Changed stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
   if (client.config.owners.includes(message.author.id))
-    message.channel.send("It's now safe to close PowerShell.").then(m => process.exit(0));
+    message.channel.send("It's now safe to turn off the bot.").then(m => process.exit(0));
   // else message.channel.send("You aren't an owner!");
 };
 
